a76cd3d0cfdb2050bc325ae0bf7d744b32ebea44
Carter Pape <Carter@Carters-MacBook-Air.local>
2015-08-10 17:09:55 -0400

    Initial Commit

---
+++ Basic binary genetic optimization/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Basic binary genetic optimization
+//
+//  Created by Carter Pape on 8/10/15.
+//  Copyright (c) 2015 Carter Pape. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
